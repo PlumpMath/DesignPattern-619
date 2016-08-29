e743efe158b1f26bed2f05276b617746b1474ff5
xsh <xsh@xsh-HP>
2016-08-29 11:41:40 +0800

    Change-Id: Ic4a69a6afc44ef62ff0174ae500fc64aff7a0dde Signed-off-by: xsh <xsh@xsh-HP>

---
+++ src/iterator_pattern/DinerMenu.java
@@ -0,0 +1,38 @@
+/**
+ * @project DesignPattern
+ * @package iterator_pattern
+ * @filename DinerMenu.java
+ */
+package iterator_pattern;
+/**
+ * @author xsh
+ * @Email xshqhua@foxmail.com
+ * @date 2016年8月28日 下午10:30:53
+ */
+public class DinerMenu {
+	static final int MAX_ITEMS = 6;
+	int numberOfItem = 0;
+	MenuItem[] menuItems;
+	public DinerMenu(){
+		menuItems = new MenuItem[MAX_ITEMS];
+		addItem("Vegetaritan BLT", "Pancakes with scrambled eggs, and toast", true, 2.99);
+		addItem("Bacon", "Pancakes with fried eggs", false, 2.3);
+		addItem("Soup", "Pancakes with toast", true, 3.1);
+		addItem("Hotdog", "scrambled eggs, and toast", true, 2.5);
+	}
+	public void addItem(String name,
+			String description,
+			boolean vegetarian,
+			double price){
+		if(numberOfItem>MAX_ITEMS){
+			System.out.println("Out of MenuItem is full;");
+			return;
+		}
+		MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+		menuItems[numberOfItem] = menuItem;
+		numberOfItem+=1;
+	}
+	public MenuItem[] getMenuItems(){
+		return menuItems;
+	}
+}
